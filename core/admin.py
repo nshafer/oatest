a5c48ab94dc4c024022281425d69c522c971a877
Nathan Shafer <nate@lotech.org>
2022-11-04 09:06:19 -0700

    Setup failure condition

--- core/admin.py
+++ core/admin.py
@@ -1,9 +1,28 @@
 from django.contrib import admin
+from django_object_actions import DjangoObjectActions
 
 from core.models import Article
 
 
 @admin.register(Article)
-class ArticleAdmin(admin.ModelAdmin):
+class ArticleAdmin(DjangoObjectActions, admin.ModelAdmin):
     list_display = ('id', 'title', 'published')
+    change_actions = ['publish_this']
 
+    def get_change_actions(self, request, object_id, form_url):
+        actions = super().get_change_actions(request, object_id, form_url)
+        actions = list(actions)
+
+        # Uncomment this to unquote the object_id
+        # from django.contrib.admin.utils import unquote
+        # object_id = unquote(object_id)
+
+        print("object_id", object_id)
+        obj = self.model.objects.get(pk=object_id)
+        if obj.published:
+            actions.remove('publish_this')
+        return actions
+
+    def publish_this(self, request, obj):
+        obj.published = True
+        obj.save(update_fields=['published'])
